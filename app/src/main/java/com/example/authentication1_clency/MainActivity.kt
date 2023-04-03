75afd1a5ff1ef8bc1b49224ab340d6ed86fb7aa7
emobilis <clency2023@gmail.com>
2023-04-03 10:29:39 +0300

    authentication app

---
+++ app/src/main/java/com/example/authentication1_clency/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.authentication1_clency
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
